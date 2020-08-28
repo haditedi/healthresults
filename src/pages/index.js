fd4abd52cbb7a49a350141fbd9cb41d7a7de21fd
Hadi Tedi <haditedi@gmail.com>
2020-08-28 14:06:54 +0100

    added s at Index

--- src/pages/index.js
+++ src/pages/index.js
@@ -126,7 +126,7 @@ const Index = () => {
             data-sal-duration="2000"
           >
             When you look for a solution in health, business and our hectic
-            time-pressed lives- what matters most? RESULT!
+            time-pressed lives- what matters most? RESULTS!
           </h1>
         </section>
       </div>
